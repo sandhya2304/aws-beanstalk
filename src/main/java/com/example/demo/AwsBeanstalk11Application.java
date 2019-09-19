1758334cb1af3c267d871a51014eb67efe0fab71
sandhya0423 <sandhyasharma0423@gmail.com>
2019-09-19 13:40:16 +0530

    commist beanstalk

---
+++ src/main/java/com/example/demo/AwsBeanstalk11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBeanstalk11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBeanstalk11Application.class, args);
+	}
+
+}
